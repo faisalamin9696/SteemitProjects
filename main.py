157c744d3d0ef4b77bde5f81dc7a93002851732f
M.Faisal Amin <faisalamin9696@gmail.com>
2022-07-06 16:57:00 +0500

    project updated with gunicorn

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ app = Flask(__name__)
 # main file
 @app.route('/')
 def home_page():
-    return "Hello World"
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
     from waitress import serve
-
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0")
 
 
 @app.route('/active_posts')
 def active_posts_page():
-    return "hello, World"
+    response = requests.get(
+        url=f'https://sds0.steemworld.org/feeds_api/getActiveCommunityPostsByCreated/hive-144064/faisalamin/50/1000').json()
+    cols_res = response['result']['cols']
+    rows_res = response['result']['rows']
+    return render_template('active_posts.html', items_cols=cols_res, items_rows=rows_res)
