69d1796d4e16f7a98d82829c0fed9de8d33469c0
M.Faisal Amin <faisalamin9696@gmail.com>
2022-07-06 11:35:38 +0500

    project updated with gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from main import app
+import os
 
 if __name__ == "__main__":
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
