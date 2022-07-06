fbc6b9ed68cc4057551047f149de39833021088f
M.Faisal Amin <faisalamin9696@gmail.com>
2022-07-06 16:51:06 +0500

    project updated with gunicorn

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # main file
 @app.route('/')
 def home_page():
-    return render_template('home.html')
+    return "Hello World"
 
 
 if __name__ == '__main__':
@@ -19,8 +19,4 @@ if __name__ == '__main__':
 
 @app.route('/active_posts')
 def active_posts_page():
-    response = requests.get(
-        url=f'https://sds0.steemworld.org/feeds_api/getActiveCommunityPostsByCreated/hive-144064/faisalamin/50/1000').json()
-    cols_res = response['result']['cols']
-    rows_res = response['result']['rows']
-    return render_template('active_posts.html', items_cols=cols_res, items_rows=rows_res)
+    return "hello, World"
