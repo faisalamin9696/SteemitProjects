2fc2b533f7efe2d6b4f3e4ede866f5a3ce551e7d
faisalamin9696 <53206866+faisalamin9696@users.noreply.github.com>
2022-07-06 09:28:04 +0500

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get(url='https://sds.steemworld.org/communities_api/getCommunitiesByCountActivePosts').json()
-cols_res = response['result']['cols']
-rows_res = response['result']['rows']
-
-print(rows_res)
